38acfe4456e3af58e0dc2f8bfae8329ed12ea7f3
Izxy86 <gamboggimartin@gmail.com>
2023-04-10 18:53:56 -0300

    ..

--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -7,14 +7,7 @@ const productManager = new ProductManager('./DB.json')
 const cartManager = new CartManager('./carts.json')
 const notFound = { error: "Cart not found" }
 
-/* ok: 200
-    created: 201
-    no content: 204
-    bad request: 400
-    forbidden: 403
-    not found: 404
-    internal server error: 500
-    */
+
 
 router.post("/", async (req, res) => {
   await cartManager.createCart()
